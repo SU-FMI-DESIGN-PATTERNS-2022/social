95d3db0b39178b829dbe5da644690f778f49d8c5
KaterinaZaharieva <katerinapzah@gmail.com>
2022-12-11 19:32:34 +0200

    add reactions to branch

--- src/main/java/com/social/BeansConfiguration.java
+++ src/main/java/com/social/BeansConfiguration.java
@@ -8,13 +8,30 @@ import com.social.users.repository.SocialUsersRepository;
 import com.social.users.service.SocialUsersService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 
 @Configuration
 public class BeansConfiguration {
 
+
   @Bean
   public CommentsService commentsService(CommentsRepository commentsRepository) {
     return new CommentsService(commentsRepository);
+  }
+
+
+  @Bean
+  public ReactionsService reactionsService(ReactionsRepository reactionRepository) {
+    return new ReactionsService(reactionRepository);
   }
 
   @Bean
--- src/main/java/com/social/reactions/api/ReactionsController.java
+++ src/main/java/com/social/reactions/api/ReactionsController.java
@@ -1,7 +1,12 @@
 package com.social.reactions.api;
 
 import com.social.api.ReactionsApi;
+import com.social.model.ParentType;
 import com.social.model.ReactionDto;
+import com.social.reactions.model.Reaction;
+import com.social.reactions.model.ReactionType;
+import com.social.reactions.service.ReactionsService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,14 +14,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class ReactionsController implements ReactionsApi {
+  private final ReactionsService reactionsService;
+
+  public ReactionsController(ReactionsService reactionsService) {
+    this.reactionsService = reactionsService;
+  }
 
   @Override
-  public ResponseEntity<Void> deleteReactionsReactionId(String reactionId) {
-    return null;
+  public ResponseEntity<Void> deleteReactionsReactionId(String parentId) {
+    String userId = "JFDOJ";
+    reactionsService.deleteReaction(userId,parentId);
+    return new ResponseEntity<>(HttpStatus.OK);
   }
 
   @Override
   public ResponseEntity<ReactionDto> putReactions(ReactionDto reactionDto) {
-    return null;
+    Reaction reaction = peactionDtoToReaction(reactionDto);
+    if(!reactionsService.doesParentExist(reaction.getParentId())) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    reactionsService.createReaction(reaction);
+    return new ResponseEntity<>(HttpStatus.CREATED);
+  }
+
+  private Reaction peactionDtoToReaction(ReactionDto reactionDto)
+  {
+    String userId = "JFDOJ";
+    String parentId = reactionDto.getParentId();
+    ParentType parentType = ParentType.valueOf(reactionDto.getParentType());
+    ReactionType reactionType = ReactionType.valueOf(reactionDto.getReactionType().toString());
+    return new Reaction(userId,parentId,parentType,reactionType);
   }
 }
